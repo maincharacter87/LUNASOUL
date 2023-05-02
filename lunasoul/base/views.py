533c8ca8b4033b2bf6f49f1210c18084dca0e36f
Jane Swingler <jswingler007@gmail>
2023-05-02 16:32:09 +0200

    Added doctring to shop view function

--- lunasoul/base/views.py
+++ lunasoul/base/views.py
@@ -2,14 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    """
-    Renders the 'index.html' template.
-
-    Args:
-        request (HttpRequest): The HTTP request object.
-
-    Returns:
-        HttpResponse: A rendered HTTP response.
-
-    """
     return render(request, 'index.html')
