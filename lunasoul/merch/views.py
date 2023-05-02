c2ea9663a24a93373840d8099e4d7577b30ccf34
Jane Swingler <jswingler007@gmail>
2023-05-02 16:28:55 +0200

    Added documentation for the shop view function

--- lunasoul/merch/views.py
+++ lunasoul/merch/views.py
@@ -4,4 +4,14 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def shop(request):
+    """
+    Renders the 'shop.html' template if the user is logged in.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A rendered HTTP response.
+
+    """
     return render(request, 'shop.html')
